393fe6ba8aa1c795ce066c5fe7e721a45a194ae9
Alejandro Costa <alejandroesfc@gmail.com>
2021-12-27 10:30:38 -0300

    the compiler sees polymorphic objects as an instance of the superclass

--- inheritance/TestManager.java
+++ inheritance/TestManager.java
@@ -10,7 +10,7 @@ public class TestManager
 
         var staff = new Employee[3];
 
-        staff[0] = boss; //Object variables are polymorphic. A variable of type Employee can refer to an Employee instance, but also to an instance of any subclass of Employee, such as Manager.
+        staff[0] = boss; //Object variables are polymorphic. A variable of type Employee can refer to an Employee instance, but also to an instance of any subclass of Employee, such as Manager. But... it will be considered an Employee by the compiler. So, you can use boss.setBonus(500), but you cannot use staff[0].setBonus(500), because it is seen as an Employee.
         staff[1] = new Employee("Harry Hacker", 50000, 1989, 10, 1);
         staff[2] = new Employee("Tommy Tester", 40000, 1990, 3, 15);
 
