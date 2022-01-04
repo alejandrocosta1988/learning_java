3139f8de11569675b9f0e1527cacfaaaa3b97db0
Alejandro Costa <alejandroesfc@gmail.com>
2022-01-04 08:06:14 -0300

    comments to highlight features of the Java language

--- abstractClasses/PersonTest.java
+++ abstractClasses/PersonTest.java
@@ -10,12 +10,12 @@ public class PersonTest
 {
     public static void main(String[] args)
     {
-        var people = new Person[2];
+        var people = new Person[2]; //No object of an abstract class may be created, but a variable of type Person may refer to objects of concrete subclasses.
 
-        people[0] = new Employee("Harry Hacker", 50000, 1989, 10, 1);
-        people[1] = new Student("Maria Morris", "computer science");
+        people[0] = new Employee("Harry Hacker", 50000, 1989, 10, 1); //reference to concrete subclass
+        people[1] = new Student("Maria Morris", "computer science"); //reference to concrete subclass
 
         for (Person aPerson : people)
-            System.out.println(aPerson.getName() + ", " + aPerson.getDescription());
+            System.out.println(aPerson.getName() + ", " + aPerson.getDescription()); // dynamic binding ensures the correct call of method getDescription()
     }
 }
