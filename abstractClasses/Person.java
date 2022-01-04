97c3d6c8edd01c76ebf7199fff604a651a1c6315
Alejandro Costa <alejandroesfc@gmail.com>
2022-01-04 07:55:09 -0300

    comments to highlight abstract classes features

--- abstractClasses/Person.java
+++ abstractClasses/Person.java
@@ -1,16 +1,16 @@
 package abstractClasses;
 
-public abstract class Person
+public abstract class Person //keyword: abstract --> it is used when one or more methods of the class are abstract.
 {
-    private String name;
-    public abstract String getDescription();
+    private String name; //abstract classes may have concrete fields.
+    public abstract String getDescription(); //abstract methods need no implementation. These methods must be implemented in the concrete subclasses that inherit from the abstract superclass.
 
     public Person(String name)
     {
         this.name = name;
     }
 
-    public String getName()
+    public String getName() //abstract classes may have concrete methods.
     {
         return this.name;
     }
