0faaeb4fa4bc0b8dc8618988adfb273903610d89
Alejandro Costa <alejandroesfc@gmail.com>
2022-01-05 08:18:18 -0300

    Basic implementation of class Employee

---
+++ equals/Employee.java
@@ -0,0 +1,18 @@
+package equals;
+
+import java.time.*;
+import java.util.Objects;
+
+public class Employee
+{
+    private String name;
+    private double salary;
+    private LocalDate hireDay;
+
+    public Employee(String name, double salary, int year, int month, int day)
+    {
+        this.name = name;
+        this.salary = salary;
+        this.hireDay = LocalDate.of(year, month, day);
+    }
+}
