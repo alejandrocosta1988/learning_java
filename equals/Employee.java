b0e6b299a4d810ece3447d9a670bec21e4aa9eff
Alejandro Costa <alejandroesfc@gmail.com>
2022-01-05 08:36:35 -0300

    implements method equals()

--- equals/Employee.java
+++ equals/Employee.java
@@ -15,4 +15,24 @@ public class Employee
         this.salary = salary;
         this.hireDay = LocalDate.of(year, month, day);
     }
+
+    public boolean equals(Object otherObject)
+    {
+        //check if the objects are identical
+        if (this == otherObject) return true;
+
+        //returns false if explicit parameter is null
+        if (otherObject == null) return false;
+
+        //if the classes don't match, they can't be equal
+        if (this.getClass() != otherObject.getClass()) return false;
+
+        //otherObject is a non-null Employee
+        var other = (Employee) otherObject;
+
+        //check if the fields have identical values
+        return Objects.equals(this.name, other.name)
+          && this.salary == other.salary
+          && Objects.equals(this.hireDay, other.hireDay);
+    }
 }
