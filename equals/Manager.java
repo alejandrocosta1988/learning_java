0ccdd3baac4a9a67d527e1509b975e3edb73a639
Alejandro Costa <alejandroesfc@gmail.com>
2022-01-16 10:32:14 -0300

    defines the class Manager

---
+++ equals/Manager.java
@@ -0,0 +1,12 @@
+package equals;
+
+public class Manager extends Employee
+{
+    private double bonus;
+
+    public Manager(String name, double salary, int year, int month, int day)
+    {
+        super(name, salary, year, month, day);
+        this.bonus = 0;
+    }
+}
